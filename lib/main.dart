5d05ce024414d3ca3c4d7f084b6e826a0e5065ff
Jonathan <tobingkagum29@gmail.com>
2026-06-06 16:52:49 +0700

    Nambah page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:korlap_online_upi/pages/homeview/user_home_view.dart';
+import 'package:korlap_online_upi/pages/login/login_page.dart';
 import 'package:korlap_online_upi/session_provider.dart';
 import 'package:korlap_online_upi/test.dart';
 import 'package:korlap_online_upi/widgets/auth_gate.dart';
@@ -28,11 +30,27 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Korlap Digital (Kodig)',
       debugShowCheckedModeBanner: false,
-
       //TODO: Nanti ini diganti ke AuthGate() default nya 
-      
       // TestPage() untuk nge test page
-      home: const TestPage(),
+      home: const LoginPage(),
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
     );
   }
 }
